4049658d3f8eca2013352c76c7fe6b7bf587a02f
Englert Ervin  Brunó <englert@users.noreply.github.com>
2024-01-24 13:50:03 +0000

    nemjoazadatokmodosítasa

--- pages/módosítás.py
+++ pages/módosítás.py
@@ -26,16 +26,24 @@ def main():
         df = pd.DataFrame(data, columns=["ID", "Firstname", "Lastname", "Csipszam", "Ivar", "Fajta", "Egeszsegi_allapot", "Fogazat", "Kor", "Viselkedes", "Egyeb_jellemzok"])
 
         # Táblázat megjelenítése
-        selected_row = st.radio("Select a row to modify:", df['ID'].tolist())
+        st.dataframe(df)
+
+        selected_row = st.number_input("Select a row to modify:", min_value=1, max_value=len(df), value=1, step=1)
 
         # A kiválasztott sor megjelenítése
-        selected_data = df[df['ID'] == selected_row]
-        st.dataframe(selected_data)
+        selected_data = df.iloc[selected_row - 1]
+        st.subheader("Selected Row:")
+        st.write(selected_data)
 
         if st.button("Modify Data"):
             # Módosítás logikája
-            new_firstname = st.text_input("New Firstname", selected_data['Firstname'].iloc[0])
-            new_lastname = st.text_input("New Lastname", selected_data['Lastname'].iloc[0])
+            new_firstname = st.text_input("New Firstname", selected_data['Firstname'])
+            new_lastname = st.text_input("New Lastname", selected_data['Lastname'])
+            # ... Az összes többi módosítandó adatot itt adja meg
+
+            # Adatok módosítása a DataFrame-ben
+            df.loc[selected_row - 1, 'Firstname'] = new_firstname
+            df.loc[selected_row - 1, 'Lastname'] = new_lastname
             # ... Az összes többi módosítandó adatot itt adja meg
 
             # Adatok módosítása az SQLite adatbázisban
@@ -43,10 +51,14 @@ def main():
             c = conn.cursor()
 
             c.execute("UPDATE allatok SET firstname=?, lastname=?, csipszam=?, ivar=?, fajta=?, egeszsegi_allapot=?, fogazat=?, kor=?, viselkedes=?, egyeb_jellemzok=? WHERE id=?",
-                      (new_firstname, new_lastname, selected_data['Csipszam'].iloc[0], selected_data['Ivar'].iloc[0], selected_data['Fajta'].iloc[0], selected_data['Egeszsegi_allapot'].iloc[0], selected_data['Fogazat'].iloc[0], selected_data['Kor'].iloc[0], selected_data['Viselkedes'].iloc[0], selected_data['Egyeb_jellemzok'].iloc[0], selected_row))
+                      (new_firstname, new_lastname, selected_data['Csipszam'], selected_data['Ivar'], selected_data['Fajta'], selected_data['Egeszsegi_allapot'], selected_data['Fogazat'], selected_data['Kor'], selected_data['Viselkedes'], selected_data['Egyeb_jellemzok'], selected_row))
 
             conn.commit()
             conn.close()
+
+            # Táblázat frissítése
+            st.dataframe(df)
+
             st.success("Adatok sikeresen módosítva")
 
     else:
